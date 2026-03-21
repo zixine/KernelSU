78b72a6261317f946707998cb36dd72e4b95b1e1
zixine <waheid005@gmail.com>
2026-03-21 15:42:15 +0700

    Update custom.sh

--- manager/custom.sh
+++ manager/custom.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Kita ubah variabel ini menjadi zixine dan zixinesu
 word1="com"
-word2="rapli"
-word3="mambosu"
+word2="zixine"
+word3="zixinesu"
 
 # Export variables for use in find -exec
 export word1 word2 word3
 
-# Rename directories
+# Rename directories (mengubah struktur folder app)
 find . -depth -type d -name 'me' -execdir mv {} "$word1" \;
 find . -depth -type d -name 'weishu' -execdir mv {} "$word2" \;
 find . -depth -type d -name 'kernelsu' -execdir mv {} "$word3" \;
 
-# Replace inside files
+# Replace inside files (mengubah referensi package di dalam semua file)
 find . -type f -exec sed -i \
     -e "s/me\.weishu\.kernelsu/$word1.$word2.$word3/g" \
     -e "s/me\/weishu\/kernelsu/$word1\/$word2\/$word3/g" \
     -e "s/me_weishu_kernelsu/${word1}_${word2}_${word3}/g" {} +
 
+# Mengubah nama file APK yang dihasilkan saat di-build
 if [ -f "./app/build.gradle.kts" ]; then
-    sed -i 's/outputFileName = "KernelSU_${managerVersionName}_${managerVersionCode}-\$name.apk"/outputFileName = "MamboSU_${managerVersionName}_${managerVersionCode}-\$name.apk"/' ./app/build.gradle.kts
+    # Jika sebelumnya bernama KernelSU
+    sed -i 's/outputFileName = "KernelSU_${managerVersionName}_${managerVersionCode}-\$name.apk"/outputFileName = "ZixineSu_${managerVersionName}_${managerVersionCode}-\$name.apk"/' ./app/build.gradle.kts
+    # Jika sebelumnya bernama MamboSU
+    sed -i 's/outputFileName = "MamboSU_${managerVersionName}_${managerVersionCode}-\$name.apk"/outputFileName = "ZixineSu_${managerVersionName}_${managerVersionCode}-\$name.apk"/' ./app/build.gradle.kts
 fi
 
-echo "Done."
+echo "Done. Berhasil mengubah menjadi ZixineSu."
